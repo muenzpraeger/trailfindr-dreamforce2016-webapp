18a6c452065bcaeebefc4b350a850870bb65feaf
René Winkelmeyer <mail@winkelmeyer.com>
2016-09-24 10:16:47 +0200

    Added feedback route

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require('express');
-var http = require('http');
-var https = require('https');
-var fs = require('fs');
 var env = require('node-env-file');
+var bodyParser = require('body-parser')
 var queries = require('./node/javascript/queries.js');
+var inputs = require('./node/javascript/inputs.js');
 
 // Load environment variables for localhost
 try {
@@ -18,6 +17,7 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/node/ejs/pages');
 
 app.use(express.static(__dirname + '/node'));
+app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
 	res.render('index', {});
@@ -26,6 +26,7 @@ app.get('/', function(req, res) {
 app.get('/api/beacons', queries.getBeacons);
 app.get('/api/venue', queries.getVenue);
 app.get('/api/graph', queries.getGraph);
+app.post('/api/feedback', inputs.setFeedback);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
